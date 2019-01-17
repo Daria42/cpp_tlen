bbdb8c48563e3bdc2306e2a7ae823f224e047417
Danil Tsvirkun <30415253+tsvdan@users.noreply.github.com>
2019-01-17 17:51:44 +0300

    Убрал ссылку

--- petrova_d_a/prj.labs/matrix/matrix.h
+++ petrova_d_a/prj.labs/matrix/matrix.h
@@ -10,7 +10,7 @@ class Matrix {
     ~Matrix();
     ptrdiff_t const rows() const { return row_; }
     ptrdiff_t const columns() const { return col_; }
-    int& get (const ptrdiff_t i, const ptrdiff_t j) const;
+    int get (const ptrdiff_t i, const ptrdiff_t j) const;
     int& get (const ptrdiff_t i, const ptrdiff_t j);
     Matrix& operator=(const Matrix& mat);
 
